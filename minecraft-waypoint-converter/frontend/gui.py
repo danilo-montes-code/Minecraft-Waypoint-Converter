8893771adf08f583ee6995699b6bdeedb2a21df7
danilo-montes <dem9855@nyu.edu>
2025-07-26 10:29:44 -0400

    various gui testing

--- minecraft-waypoint-converter/frontend/gui.py
+++ minecraft-waypoint-converter/frontend/gui.py
@@ -8,17 +8,70 @@ from PyQt6.QtWidgets import (
     QWidget,
     QComboBox,
 )
+from PyQt6 import uic, QtWidgets
+import sys
+from pathlib import Path
+
+basedir = Path(__file__).parent
+print(basedir)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = uic.loadUi(Path(
+    basedir,
+    "MinecraftWaypointConverterMainWindow.ui"
+    # ".ui"
+))
+window.show()
+app.exec()
 
 
 
-class MinecraftWaypointConverterMainWindow(QMainWindow):
-    """
-    A class that create the GUI for Minecraft Waypoint Converter.
-    """
+
+
+
+
+
+
+
+# from MCWPCMW import Ui_MainWindow
+
+# import sys
+# from PyQt6 import QtWidgets, uic
+
+# class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+#     def __init__(self, *args, obj=None, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.setupUi(self)
+
+
+# app = QtWidgets.QApplication(sys.argv)
+
+# window = MainWindow()
+# window.show()
+# app.exec()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class MinecraftWaypointConverterMainWindow(QMainWindow):
+#     """
+#     A class that create the GUI for Minecraft Waypoint Converter.
+#     """
     
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Minecraft Waypoint Converter")
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowTitle("Minecraft Waypoint Converter")
 
-        container = QWidget()
-        self.setCentralWidget(container)
+#         container = QWidget()
+#         self.setCentralWidget(container)
