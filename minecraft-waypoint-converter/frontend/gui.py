9b2026d170fd4aefd76ee053867f5797ff2071aa
danilo-montes <dem9855@nyu.edu>
2025-08-09 23:40:30 -0400

    basic ui v1

--- minecraft-waypoint-converter/frontend/gui.py
+++ minecraft-waypoint-converter/frontend/gui.py
@@ -3,54 +3,54 @@
 Contains a class that creates the GUI for Minecraft Waypoint Converter.
 """
 
-from PyQt6.QtWidgets import (
-    QMainWindow,
-    QWidget,
-    QComboBox,
-)
-from PyQt6 import uic, QtWidgets
-import sys
-from pathlib import Path
-
-basedir = Path(__file__).parent
-print(basedir)
-
-
-app = QtWidgets.QApplication(sys.argv)
-
-window = uic.loadUi(Path(
-    basedir,
-    "MinecraftWaypointConverterMainWindow.ui"
-    # ".ui"
-))
-window.show()
-app.exec()
-
-
-
-
-
-
-
-
-
-
-# from MCWPCMW import Ui_MainWindow
-
+# from PyQt6.QtWidgets import (
+#     QMainWindow,
+#     QWidget,
+#     QComboBox,
+# )
+# from PyQt6 import uic, QtWidgets
 # import sys
-# from PyQt6 import QtWidgets, uic
+# from pathlib import Path
 
-# class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-#     def __init__(self, *args, obj=None, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.setupUi(self)
+# basedir = Path(__file__).parent
+# print(basedir)
 
 
 # app = QtWidgets.QApplication(sys.argv)
 
-# window = MainWindow()
+# window = uic.loadUi(Path(
+#     basedir,
+#     "MinecraftWaypointConverterMainWindow.ui"
+#     # ".ui"
+# ))
 # window.show()
 # app.exec()
+
+
+
+
+
+
+
+
+
+
+from test import Ui_MainWindow
+
+import sys
+from PyQt6 import QtWidgets, uic
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
 
 
 
